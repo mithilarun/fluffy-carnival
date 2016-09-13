1a33ef51f45c833aa269971cd68fcda969d7d5ae
Mithil Arun <marun@asu.edu>
2016-09-13 22:16:43 +0000

    Added test and packaging infrastructure

---
+++ setup.py
@@ -0,0 +1,28 @@
+#
+# Copyright (c) 2016 Mithil Arun
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This file acts as a setup module for the OpenStack Automatic Network
+# Bandwidth Management Service.
+#
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ops-bw-mgr",
+    version="0.0.1",
+    description="Automatic OpenStack Bandwidth Management Project",
+    license="GPL3",
+)
