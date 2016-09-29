d2a01ef91ed39b66c0b365d2edd099443965fdbd
Alan Rodrigues <avrodri9@asu.edu>
2016-09-29 12:14:59 -0700

    data collection

--- dataCollection.py
+++ dataCollection.py
@@ -1,6 +1,14 @@
 import ConfigParser
 import MySQLdb
+import os
 
+dir_path=os.path.dirname(os.path.realpath(__file__))
+fname=dir_path+"/data.txt"
+if os.path.isfile(fname):
+	os.remove(fname)
+	f=open(fname,"w")
+else:
+	f=open(fname,"w") 
 CONFIG_FILE="/home/ubuntu/code/BW_ctrl_ml/connection.conf"
 config = ConfigParser.RawConfigParser()
 def ConfigSectionMap(section):
@@ -36,8 +44,12 @@ for row in cur.fetchall():
 		instDict[row[6]].append(row[17])
 	else:
 		instDict[row[6]]=[row[17]]
-	
-print instDict
+
+for it in instDict.items():
+	f.write("%s:%s" %it)
+f.write("\n")
+#f.write(instDict)
+#f.write("\n")
 
 db = MySQLdb.connect(host=ConfigSectionMap("Controller")['address'],
                      user=ConfigSectionMap("Controller")['username'],
@@ -57,6 +69,9 @@ for row in cur.fetchall():
                 		instPortDict[row[0]].append(row[1].split('-')[0])
         		else:
                 		instPortDict[row[0]]=[row[1].split('-')[0]]
-print instPortDict
+for it in instPortDict.items():
+        f.write("%s:%s" %it)
+f.write("\n")
+#fwrite(instPortDict)
 db.close()
 
