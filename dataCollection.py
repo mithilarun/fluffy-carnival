f8c339d89c7d6166dcc9ee8c307d5955ff6ce1e6
Alan Rodrigues <avrodri9@asu.edu>
2016-09-29 00:11:45 -0700

    adding files

--- dataCollection.py
+++ dataCollection.py
@@ -1,48 +1,43 @@
 import ConfigParser
+import MySQLdb
 
+CONFIG_FILE="/home/ubuntu/code/BW_ctrl_ml/connection.conf"
 config = ConfigParser.RawConfigParser()
 def ConfigSectionMap(section):
-	config.read("/home/ubuntu/code/BW_ctrl_ml/connection.conf")
+	config.read(CONFIG_FILE)
 	dict1 = {}
 	try:
 		options = config.options(section)
     		for option in options:
 			try:
 				dict1[option] = config.get(section, option)
-				#if dict1[option] == -1:
-				#    DebugPrint("skip: %s" % option)
 			except:
 				print("exception on {]".format(option))
 				dict1[option] = None
 	except:
 		print("exception on {}".format(section))
     	return dict1	
-#!/usr/bin/python
-import MySQLdb
 
+
+instDict=dict()
 db = MySQLdb.connect(host=ConfigSectionMap("Controller")['address'],
                      user=ConfigSectionMap("Controller")['username'],   
                      passwd=ConfigSectionMap("Controller")['password'],
                      db="nova",
 		     port=3306)
 
-#db = mysql.connector.connect( host='192.168.1.7',
-#          	      user='ubuntu',   
-#                      passwd='123456',
-#                     db="keystone",
-#                    port=3306)
-
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
+#cursor object to execute command
 cur = db.cursor()
 
-# Use all the SQL you like
 cur.execute("SELECT * FROM instances")
 
-# print all the first cell of all the rows
 for row in cur.fetchall():
-	print row
-	print '\n'
+	if row[6] in instDict:
+		instDict[row[6]].append(row[17])
+	else:
+		instDict[row[6]]=[row[17]]
+	
+print instDict
 
 db = MySQLdb.connect(host=ConfigSectionMap("Controller")['address'],
                      user=ConfigSectionMap("Controller")['username'],
@@ -52,17 +47,16 @@ db = MySQLdb.connect(host=ConfigSectionMap("Controller")['address'],
 
 cur = db.cursor()
 
-# Use all the SQL you like
 cur.execute("SELECT * FROM ports")
 
-# print all the first cell of all the rows
+instPortDict=dict()
 for row in cur.fetchall():
-        print row
-        print '\n'
-
+	if "compute:nova" in row[8]:
+		if row[0] in instDict:
+			if row[0] in instPortDict:
+                		instPortDict[row[0]].append(row[1].split('-')[0])
+        		else:
+                		instPortDict[row[0]]=[row[1].split('-')[0]]
+print instPortDict
 db.close()
-#Name = ConfigSectionMap("Controller")['name']
-#IP = ConfigSectionMap("Compute")['address']
-#print Name
-#print IP
 
